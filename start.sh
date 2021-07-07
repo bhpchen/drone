5e80f04c7b38249704c6dde0574ac079b6a9dc68
terrylai <cellterry@gmail.com>
2021-07-07 15:20:48 +0800

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 docker stop drone
 docker rm drone
 docker build --tag drone-docker .
-docker run --gpus all -p 5000:5000 -d -v $(pwd)/detect.py:/usr/src/app/detect.py -v $(pwd)/cut_video.py:/usr/src/app/cut_video.py -v $(pwd)/app.py:/usr/src/app/app.py --name drone drone-docker
+docker run --gpus all --ipc=host -p 5000:5000 -d -v $(pwd)/detect.py:/usr/src/app/detect.py -v $(pwd)/cut_video.py:/usr/src/app/cut_video.py -v $(pwd)/app.py:/usr/src/app/app.py --name drone drone-docker
 #docker run --gpus all --ipc=host --entrypoint "" -it -v $(pwd)/detect.py:/usr/src/app/detect.py -v $(pwd)/cut_video.py:/usr/src/app/cut_video.py -v $(pwd)/app.py:/usr/src/app/app.py --name flask python-docker bash
 #docker run --ipc=host --entrypoint "" -it --name flask python-docker bash
 #docker run --gpus all -p 5000:5000 --entrypoint "" -it -v $(pwd)/detect.py:/usr/src/app/detect.py -v $(pwd)/cut_video.py:/usr/src/app/cut_video.py -v $(pwd)/app.py:/usr/src/app/app.py --name drone drone-docker bash
